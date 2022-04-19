881b5fbc31bc6d9df786cd7dd1b982e2628e8ec5
rx570rohit <101522761+rx570rohit@users.noreply.github.com>
2022-04-19 18:13:56 +0530

    Add README.md.

---
+++ source/repos/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,14 @@
+﻿//See https://aka.ms/new-console-template for more information
+using System;
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        static void Main(String[] arg)
+        {
+            Program.emp();
+        }
+
+
+    }
+}
